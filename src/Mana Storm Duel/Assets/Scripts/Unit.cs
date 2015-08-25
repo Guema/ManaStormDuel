24bd36b1879780fef611327e631868c5cc5ab81a
Guema <floorent1993@gmail.com>
2015-08-25 18:59:51 +0200

    Ajout de scripts vides permettant de poser les bases du gameplay, refonte d'un model et texturing.

---
+++ src/Mana Storm Duel/Assets/Scripts/Unit.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Unit : NetworkBehaviour {
+
+    [SerializeField]
+    int MaxHealth = 100;
+    [SerializeField]
+    int Hp = 100;
+
+    [SerializeField]
+    int MaxSpeed = 100;
+    [SerializeField]
+    int Speed = 100;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ src/Mana Storm Duel/Assets/Scripts/Weapon.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Weapon : NetworkBehaviour {
+
+    [SerializeField]
+    int Range = 10;
+    [SerializeField]
+    int MaxDamage = 100;
+    [SerializeField]
+    int Damage = 100;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
